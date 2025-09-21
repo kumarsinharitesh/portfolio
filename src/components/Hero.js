91e6a7ae899845a4052f22ca6be6148fd5f0cfe2
kumarsinharitesh <156118194+kumarsinharitesh@users.noreply.github.com>
2025-09-22 02:26:44 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -120,7 +120,7 @@ const Hero = () => {
               transition={{ duration: 0.5, delay: 0.3 }}
             >
               <img
-                src="https://media.licdn.com/dms/image/v2/D5635AQHtVfhCQrQzyQ/profile-framedphoto-shrink_400_400/B56Zcs6D9sHgAk-/0/1748805108208?e=1757714400&v=beta&t=PDxpxIirbCCT4_Nyab8pmDgKUyTmhpJ1rzxle6FxPNw"
+                src="https://media.licdn.com/media/AAYQAQSOAAgAAQAAAAAAAB-zrMZEDXI2T62PSuT6kpB6qg.png"
                 alt="Ritesh Kumar Sinha"
                 className="profile-img"
                 onError={(e) => {
